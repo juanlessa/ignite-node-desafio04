6fa047c453dd3d92ecb3d4a9941249cffd890349
Juan Lessa <juanvlessa@ua.pt>
2021-04-28 12:53:49 +0100

    finished challenge

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -2,41 +2,72 @@ import { User } from "../../model/User";
 import { IUsersRepository, ICreateUserDTO } from "../IUsersRepository";
 
 class UsersRepository implements IUsersRepository {
-  private users: User[];
+    private users: User[];
 
-  private static INSTANCE: UsersRepository;
+    private static INSTANCE: UsersRepository;
 
-  private constructor() {
-    this.users = [];
-  }
-
-  public static getInstance(): UsersRepository {
-    if (!UsersRepository.INSTANCE) {
-      UsersRepository.INSTANCE = new UsersRepository();
+    private constructor() {
+        this.users = [];
     }
 
-    return UsersRepository.INSTANCE;
-  }
+    public static getInstance(): UsersRepository {
+        if (!UsersRepository.INSTANCE) {
+            UsersRepository.INSTANCE = new UsersRepository();
+        }
 
-  create({ name, email }: ICreateUserDTO): User {
-    // Complete aqui
-  }
+        return UsersRepository.INSTANCE;
+    }
 
-  findById(id: string): User | undefined {
-    // Complete aqui
-  }
+    create({ name, email }: ICreateUserDTO): User {
+        // Complete aqui
+        const user = new User();
+        Object.assign(user, {
+            name,
+            email,
+            admin: false,
+            created_at: new Date(),
+            updated_at: new Date(),
+        });
+        this.users.push(user);
+        return user;
+    }
 
-  findByEmail(email: string): User | undefined {
-    // Complete aqui
-  }
+    findById(id: string): User | undefined {
+        // Complete aqui
+        const userExistis = this.users.find((user) => user.id === id);
 
-  turnAdmin(receivedUser: User): User {
-    // Complete aqui
-  }
+        if (!userExistis) {
+            return undefined;
+        }
+        return userExistis;
+    }
 
-  list(): User[] {
-    // Complete aqui
-  }
+    findByEmail(email: string): User | undefined {
+        // Complete aqui
+        const userExistis = this.users.find((user) => user.email === email);
+
+        if (!userExistis) {
+            return undefined;
+        }
+        return userExistis;
+    }
+
+    turnAdmin(receivedUser: User): User {
+        // Complete aqui
+        const userIndex = this.users.findIndex(
+            (user) => user.id === receivedUser.id
+        );
+        if (userIndex === -1) {
+            throw new Error("user not found");
+        }
+
+        this.users[userIndex].admin = true;
+        return this.users[userIndex];
+    }
+
+    list(): User[] {
+        return this.users;
+    }
 }
 
 export { UsersRepository };
--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -2,15 +2,25 @@ import { User } from "../../model/User";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
-  user_id: string;
+    user_id: string;
 }
 
 class ListAllUsersUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+    constructor(private usersRepository: IUsersRepository) {}
 
-  execute({ user_id }: IRequest): User[] {
-    // Complete aqui
-  }
+    execute({ user_id }: IRequest): User[] {
+        // Complete aqui
+        const user = this.usersRepository.findById(user_id);
+        if (!user) {
+            throw new Error(`User ${user_id} not found`);
+        }
+
+        if (!user.admin) {
+            throw new Error("this user is not an admin");
+        }
+        const users = this.usersRepository.list();
+        return users;
+    }
 }
 
 export { ListAllUsersUseCase };
